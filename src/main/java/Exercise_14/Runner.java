b3e8f21990275278ae29dea6db786005c0bb2b82
andrej <andrej.vencelj.007@gmail.com>
2024-02-24 19:02:32 +0100

    Exercise 14 3/10

---
+++ src/main/java/Exercise_14/Runner.java
@@ -0,0 +1,16 @@
+package Exercise_14;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        Task2 magic = new Task2();
+
+        magic.generateRandomArray(10, 20);
+        System.out.println(Arrays.toString(magic.array));
+
+        magic.insertionSort();
+        System.out.println(Arrays.toString(magic.array));
+        System.out.println(magic.counter);
+    }
+}
---
+++ src/main/java/Exercise_14/Task2.java
@@ -0,0 +1,62 @@
+package Exercise_14;
+
+public class Task2 extends Task1{
+    int counter = 0;
+    public void resetCounter() {
+        counter = 0;
+    }
+    @Override
+    public void insertionSort() {
+        for (int i = 1; i < array.length; i++) {
+            int target = array[i];
+            int beforeTarget = i - 1;
+            while (beforeTarget >= 0 && array[beforeTarget] > target) {
+                array[beforeTarget + 1] = array[beforeTarget];
+                counter++;
+                beforeTarget--;
+            }
+            array[beforeTarget + 1] = target;
+        }
+    }
+    @Override
+    public void selectionSort() {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int targetIndex = i + 1; targetIndex < array.length; targetIndex++) {
+                if (array[targetIndex] < array[minIndex]) minIndex = targetIndex;
+                counter++;
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+    @Override
+    public void bubbleSort() {
+        boolean sorted = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                sorted = false;
+            }
+        }
+        if (sorted){
+            System.out.println("Array is sorted!");
+            return;
+        }
+
+        boolean swapped;
+        for (int i = 0; i < array.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                    counter++;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+}
