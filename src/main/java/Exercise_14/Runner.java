23753cff63704c5faa78498eeeaf59c873bbffc5
andrej <andrej.vencelj.007@gmail.com>
2024-02-27 00:09:00 +0100

    Exercise 14

--- src/main/java/Exercise_14/Runner.java
+++ src/main/java/Exercise_14/Runner.java
@@ -2,8 +2,8 @@ package Exercise_14;
 
 public class Runner {
     public static void main(String[] args) {
-        Task8 magic = new Task8();
-        magic.N = 15;
+        Task10 magic = new Task10();
+        magic.N = 20;
         magic.mainFunc();
     }
 }
